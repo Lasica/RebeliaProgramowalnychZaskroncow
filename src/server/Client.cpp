673da432b942dc1ef79bf1e51fcd8703f7919fda
Pawel <grzybek.trujacy@gmail.com>
2014-12-16 18:15:29 +0100

    Pliki Client.cpp i ClientsRegister.cpp są już tam, gdzie powinny być.

---
+++ src/server/Client.cpp
@@ -0,0 +1,91 @@
+#include "Client.hpp"
+#include <boost/asio.hpp>
+#include <string>
+
+// constructors & destructor
+Client::ClientID Client::nextID_ = 0;
+
+Client::Client(std::string nick, short unsigned int port, boost::asio::ip::address ip, statename state, std::string gameID) :
+    nickname_(nick), clientID_(Client::nextID_++), port_(port), ip_(ip), state_(new statename(state)), gameID_(gameID) {
+    ;
+}
+
+
+Client::Client(std::string nick, short unsigned int port, std::string ip, statename state, std::string gameID):
+    nickname_(nick), clientID_(Client::nextID_++), port_(port), ip_(boost::asio::ip::address::from_string(ip)), state_(new statename(state)), gameID_(gameID) {
+    ;
+}
+
+// copy c-tor copy also the unique id of every client
+Client::Client(const Client &c) :
+    nickname_(c.nickname_), clientID_(c.get_client_id()), port_(c.port_), ip_(c.ip_), state_(new statename(*(c.state_))), gameID_(c.gameID_) {
+    ;
+}
+
+Client::~Client() {
+    ;
+}
+
+
+// get methods
+std::string Client::get_nickname() const {
+    return nickname_;
+}
+
+Client::ClientID Client::get_client_id() const {
+    return clientID_;
+}
+
+short unsigned int Client::get_port() const {
+    return port_;
+}
+
+boost::asio::ip::address Client::get_ip() const {
+    return ip_;
+}
+
+std::string Client::get_ip_str() const {
+    return ip_.boost::asio::ip::address::to_string();
+}
+
+statename Client::get_state() const {
+    return *state_;
+}
+
+std::string Client::get_game_id() const {
+    return gameID_;
+}
+
+//set methods
+void Client::set_state(statename s) const {
+    state_.reset(new statename);
+    *state_=s;
+}
+
+void Client::set_game_id(std::string id) const {
+    gameID_ = id;
+}
+
+//
+bool Client::is_in_game() {
+    if(*state_ == IN_GAME)
+        return true;
+    else
+        return false;
+}
+
+//operator for std::set
+bool Client::operator<(const Client &comp) const {
+    if(this->ip_ != comp.ip_) {
+        if(this->ip_ < comp.ip_)
+            return true;
+        else
+            return false;
+    } else if(this->nickname_ < comp.nickname_)
+        return true;
+    else
+        return false;
+
+}
+
+
---
+++ src/server/ClientsRegister.cpp
@@ -0,0 +1,61 @@
+#include <set>
+#include <utility>
+
+#include "ClientsRegister.hpp"
+#include "Client.hpp"
+
+
+ClientsRegister::ClientsRegister() {
+    ;
+}
+
+ClientsRegister &ClientsRegister::instance() {
+    static ClientsRegister instance;
+    return instance;
+}
+
+ClientsRegister::ClientIt ClientsRegister::register_client(std::string nick, short unsigned int port,
+                                                                    std::string ip, statename state, std::string gameID) {
+    ClientsRegister::ClientIt it = clients_.insert(Client(nick, port, ip, state, gameID)).first; // insert returns pair<ClientIt, bool>
+
+    return it;
+}
+
+void ClientsRegister::remove_client(unsigned int id) {
+    clients_.erase(look_up_with_id(id));
+}
+
+void ClientsRegister::remove_client(std::string nick) {
+    clients_.erase(look_up_with_nickname(nick));
+}
+
+
+typedef std::set<Client>::iterator ClientIt;
+
+// look_up  functions find object with given nick or id
+ClientIt ClientsRegister::look_up_with_nickname(std::string nick) {
+    for(auto it = clients_.begin(); it != clients_.end(); ++it) {
+        if(it->get_nickname() == nick)
+            return it;
+    }
+
+    return clients_.end();
+}
+
+ClientIt ClientsRegister::look_up_with_id(Client::ClientID id) {
+    for(auto it = clients_.begin(); it != clients_.end(); ++it) {
+        if(it->get_client_id() == id)
+            return it;
+    }
+
+    return clients_.end();
+}
+
+// functions to change state of Client (IN_GAME or IN_LOBBY)
+void ClientsRegister::change_state(Client::ClientID id, statename st){
+    look_up_with_id(id)->set_state(st);
+}
+
+void ClientsRegister::change_state(std::string nick, statename st){
+    look_up_with_nickname(nick)->set_state(st);
+}
