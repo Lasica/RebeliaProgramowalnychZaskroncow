7c89e4f61559eec74faf425ba6f872cce4039daa
Lasica <dobrogo.a@gmail.com>
2015-01-17 23:39:38 +0100

    ClientsRegister jest po wskaznikach na klientow. Naprawiony GameRoomRawTest.

---
+++ src/shared/GameState.cpp
@@ -0,0 +1,11 @@
+#include "file:///mnt/sunflower/belkar/projects/studia/zpr/RebeliaProgramowalnychZaskroncow/src/shared/GameState.hpp"
+
+std::string GameState::show_content()
+{
+
+}
+
+Resource::Tag GameState::get_tag()
+{
+
+}
