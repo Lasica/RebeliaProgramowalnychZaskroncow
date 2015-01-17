7201d719476ac3f67a0ed2218246653a1f356fbc
Lasica <dobrogo.a@gmail.com>
2015-01-17 18:13:54 +0100

    ClientsRegister zabezpieczony mutexem.

--- src/server/ClientsRegister.cpp
+++ src/server/ClientsRegister.cpp
@@ -1,6 +1,8 @@
 #include <set>
 #include <utility>
 
+#include <boost/thread/locks.hpp>
+
 #include "ClientsRegister.hpp"
 #include "Client.hpp"
 #include "Address.hpp"
@@ -9,22 +11,22 @@
 ClientsRegister::ClientsRegister() { }
 
 ClientIt ClientsRegister::register_client(const Address *address, TcpPointer pointer) {
+    boost::unique_lock< boost::shared_mutex > lock(access_);
     ClientIt it = clients_.insert(Client(address, pointer)).first;
     return it;
 }
 
-void ClientsRegister::remove_client(unsigned int id) {
-    clients_.erase(look_up_with_id(id));
+inline void ClientsRegister::remove_client(unsigned int id) {
+    remove_client(look_up_with_id(id));
 }
-
 void ClientsRegister::remove_client(ClientIt it) {
+    boost::unique_lock< boost::shared_mutex > lock(access_);
     clients_.erase(it);
 }
 
 
-
-
 ClientIt ClientsRegister::look_up_with_id(ClientID id) const {
+    boost::shared_lock< boost::shared_mutex > lock(access_);
     for(auto it = clients_.begin(); it != clients_.end(); ++it) {
         if(it->get_client_id() == id)
             return it;
