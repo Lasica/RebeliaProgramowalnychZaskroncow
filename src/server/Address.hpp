3d4ee923411c47a0654b6a4b59ac098b3f9412c6
Lasica <dobrogo.a@gmail.com>
2015-01-17 02:09:49 +0100

    Zmiany w komentarzach.

--- src/server/Address.hpp
+++ src/server/Address.hpp
@@ -8,10 +8,22 @@
 #include <string>
 #include <iostream>
 
+/*
+ * Proponuje poważną zmianę, żeby zrobić klase typu ksiazka adresowa(AddressRegister), ktora bedzie mapą Adresow
+ * z kluczem rozpoznawalnym dla serwera (czy to para ip,port, czy to wskaznik na tcpconnection), zeby wszyscy mogli w
+ * krótkim czasie przetłumaczyć adres na ID. Przy czym jeśli połączenie na danym ip/porcie zostanie zerwane, to jedynie
+ * id klienta i wskaznik na tcp connection sa ustawiane na 0, a adres jest pamietany.
+ * Alternatywą dla mapy jest set z operatorem porownania z adresu, a wyszukiwanie z tego seta byloby metodami
+ * lower/upper_bound(Adres(o_zadanym_ip)).
+ * Do klasy Adres wyemigruje wskaznik na TCPConnection z Klienta, jak i kopia id Clienta ktory obecnie go posiada.
+ * Wowczas klient posiadalby jedynie wskaznik (najlepiej nie iterator, wskaznik! iterator umozliwialby dostep do "sasiednich" adresow) na swoj adres.
+ * Natomiast w rejestrze klientow byloby sortowanie klientow po id.
+ * TODO TODO TODO
+ */
 struct Address {
-    AddressIP ip;     // do zmiany później
+    AddressIP ip;
     AddressPort port;
-    Address(AddressIP Ip, AddressPort Port) : ip(Ip), port(Port) { }
+    Address(AddressIP Ip="non-defined", AddressPort Port=~0) : ip(Ip), port(Port) { }
 
     friend class boost::serialization::access;
     template<class Archive>
@@ -22,7 +34,6 @@ struct Address {
 
     bool operator<(const Address &latter) const;
 
-    Address() : ip(0), port(1) { }  // konstruktor bezparametrowy przydaje się m.in w testach i dla konstruktora bezparametrowego Packetu
 };
 
 std::ostream &operator<<(std::ostream &o, const Address &a);
--- src/server/ClientsRegister.hpp
+++ src/server/ClientsRegister.hpp
@@ -6,19 +6,17 @@
 #include "server/Client.hpp"
 #include "server/Address.hpp"
 #include "shared/Subject.h"
-
+/*
+ *
+ */
 class ClientsRegister {
   public:
     typedef std::set<Client>::iterator ClientIt;
     ClientsRegister();
     ~ClientsRegister() { }
     ClientIt register_client(Address address, TcpPointer pointer);
-    //ClientIt register_client(std::string nick, short unsigned int port,
-    //                             AddressIP ip, ClientState state, std::string gameID);  //returns registered client's iterator
-
 
     ClientIt look_up_with_id(ClientID id)    const;
-    //ClientIt look_up_with_nickname(std::string nick) const;
     //ClientIt look_up_with_address(Address addr)      const;
 
     /*
@@ -27,15 +25,13 @@ class ClientsRegister {
      * Decyzja do podjecia w momencie uzycia tych funkcji
      */
     //void change_state(ClientID id, ClientState state);
-    //void change_state(std::string nick, ClientState state);
     //void change_state(ClientIt it, ClientState state);
 
-    void remove_client(ClientID id);    //specify client with an id
-    //void remove_client(std::string nick);   //or with a nick
+    void remove_client(ClientID id);
     void remove_client(ClientIt it);
 
     ClientState get_state(const ClientIt &it)   const {
-        return it->get_state();      // game Id is in state
+        return it->get_state();
     }
     int         get_size()                      const {
         return clients_.size();
