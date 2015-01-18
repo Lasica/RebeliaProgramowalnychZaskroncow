35db4c5716f0c1be36301c4e06360427f21558cb
Lasica <dobrogo.a@gmail.com>
2015-01-18 13:30:07 +0100

    Maly test możliwości dokumentacji na podstawie klasy Packet.

--- src/shared/Packet.hpp
+++ src/shared/Packet.hpp
@@ -14,36 +14,35 @@
 
 
 class Packet {
-public:
-    // dziala, tylko konsekwencja umieszczenia go w klasie jest odnoszenie sie poprzez Packet::RESOURCE, itd.
-    /*
+  public:
+    //! Enum będący flagą stanowiącą podstawę komunikacji w naszym systemie
+    /**
      * Ten enum moze urosnac duzy, poniewaz do kazdej dodanej funkcjonalnosci,
-     * bedzie nowy tag pakietu - w przyszlosci mozna sie zastanowic jak to zautomatyzowac.
+     * bedzie nowy tag pakietu.
      */
     enum Tag {
-        UPDATED_RESOURCE,           // dane aktualizacyjne dla klienta, mozna rozpoznac zawartosc po tagu Resource
-        REGISTER_REQUEST,           // w srodku pakietu Handshake przedstawiajacy dane o kliencie.
-        CHAT_ENTRY_MESSAGE_REQUEST, // prosba o nadanie wiadomosci czatu
-        GAMEROOM_CREATE_REQUEST,    // prosba o stworzenie nowego pokoju
-        GAMEROOM_JOIN_REQUEST,      // prosba o dolaczenie do pokoju
-        GAMEROOM_LEAVE_REQUEST,     // prosba o opuszczenie pokoju
-        GAMEROOM_UPDATE_REQUEST,    // prosba o zmiane ustawien pokoju (np. ustawien gry dla hosta, a dla gracza wyrazenie gotowosci)
-        GAMEROOM_START_REQUEST,     // prosba o rozpoczecie rozgrywki
-        GAME_START_FAILURE_INFO,    // informacja dla klienta o niespelnionym rzadaniu, np. z powodu niegotowosci wszystkich graczy
-        SYNCHRONISE_REQUEST,        // prosba o wyslanie wszystkich zasobow... podejrzewam, że mozna sie bez tego obyc i zamiast tego zrobic timeouty
-        CLOCK_SYNCHRONISE,          // prosba o okreslenie czasu wzgledem serwera (sluzy tez do obliczenia round trip time)
-        GAME_STATE,                 // pakiet zawierajacy stan rozgrywki - do przekierowania dla konkretnej instancji gry
-        GAME_ACTION,                // byc moze sie przyda?
-        KEEP_ALIVE                  // ping! do ustalenia czy ktos stracil polaczenie.
+        UPDATED_RESOURCE,           //!< dane aktualizacyjne dla klienta, mozna rozpoznac zawartosc po tagu Resource
+        REGISTER_REQUEST,           //!< w srodku pakietu Handshake przedstawiajacy dane o kliencie.
+        CHAT_ENTRY_MESSAGE_REQUEST, //!< prosba o nadanie wiadomosci czatu
+        GAMEROOM_CREATE_REQUEST,    //!< prosba o stworzenie nowego pokoju
+        GAMEROOM_JOIN_REQUEST,      //!< prosba o dolaczenie do pokoju
+        GAMEROOM_LEAVE_REQUEST,     //!< prosba o opuszczenie pokoju
+        GAMEROOM_UPDATE_REQUEST,    //!< prosba o zmiane ustawien pokoju (np. ustawien gry dla hosta, a dla gracza wyrazenie gotowosci)
+        GAMEROOM_START_REQUEST,     //!< prosba o rozpoczecie rozgrywki
+        GAME_START_FAILURE_INFO,    //!< informacja dla klienta o niespelnionym rzadaniu, np. z powodu niegotowosci wszystkich graczy
+        SYNCHRONISE_REQUEST,        //!< prosba o wyslanie wszystkich zasobow... podejrzewam, że mozna sie bez tego obyc i zamiast tego zrobic timeouty
+        CLOCK_SYNCHRONISE,          //!< prosba o okreslenie czasu wzgledem serwera (sluzy tez do obliczenia round trip time)
+        GAME_STATE,                 //!< pakiet zawierajacy stan rozgrywki - do przekierowania dla konkretnej instancji gry
+        GAME_ACTION,                //!< byc moze sie przyda?
+        KEEP_ALIVE                  //!< ping! do ustalenia czy ktos stracil polaczenie.
     };
     typedef std::string StreamBuffer;
 
-    // boost::serialization potrzebuje bezparametrowego konstruktora, można go przenieść do "private"
-    Packet() { }
     Packet(Tag tag__, const Address* ad__=nullptr, Resource *content__=nullptr);
     Packet(Tag tag__, const Address* ad__, ResourcePtr content__=nullptr);
 
     friend class boost::serialization::access;
+    //! Serializacja Pakietu dotyczy tagu pakietu i zawartości (wskaźnika na Resource)
     template<class Archive>
     void serialize(Archive &ar, const unsigned int) {
         ar &content_;
@@ -66,7 +65,9 @@ public:
         return content_->show_content();
     }
 
-private:
+  private:
+    //! boost::serialization potrzebuje bezparametrowego konstruktora, można go przenieść do "private"
+    Packet() { }
     ResourcePtr content_;
     Tag tag_;
     const Address *address_;   // czy address jest potrzebny w pakiecie? - TAK!
